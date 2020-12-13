21afd13d0a57cc34dcedc0e6f703c866909abdaf
xiaohanhan <xiaohanhan1019@163.com>
2020-12-13 23:06:46 +0800

    baselines

--- baselines/CoSAN/train.py
+++ baselines/CoSAN/train.py
@@ -5,10 +5,55 @@ import os
 import pickle
 from CoSAN import CoSAN
 from dataloader import SessionDataSet
-from metric import cal_hr, cal_mrr, cal_ndcg
 import datetime
+import numpy as np
+import math
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+
+def cal_hr(predict, ground_truth):
+    """
+    calculate hit rate
+    :param predict: predict, (batch_size, k)
+    :param ground_truth: ground truth, (batch_size, )
+    :return: hr
+    """
+    hr = 0
+    for i, gt in enumerate(ground_truth):
+        if gt in predict[i]:
+            hr += 1
+    return hr
+
+
+def cal_mrr(predict, ground_truth):
+    """
+    calculate mrr
+    :param predict: predict, (batch_size, k)
+    :param ground_truth: ground truth, (batch_size, )
+    :return: mrr score
+    """
+    mrr = 0
+    for i, gt in enumerate(ground_truth):
+        if gt in predict[i]:
+            rank = np.where(predict[i] == gt)[0][0]
+            mrr += 1 / (rank + 1)
+    return mrr
+
+
+def cal_ndcg(predict, ground_truth):
+    """
+    calculate ndcg
+    :param predict: predict, (batch_size, k)
+    :param ground_truth: ground truth, (batch_size, )
+    :return: ndcg score
+    """
+    ndcg = 0
+    for i, gt in enumerate(ground_truth):
+        if gt in predict[i]:
+            rank = np.where(predict[i] == gt)[0][0]
+            ndcg += 1 / math.log(rank + 2, 2)
+    return ndcg
 
 
 def generate_config(dropouts, heads, lrs, regs, ks, session_lengths, embedding_dims, config):
